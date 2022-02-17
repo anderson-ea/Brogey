cf2d3b73eea04aee73c4c62bd6b3ceb0ad427391
Eric Anderson <59844963+anderson-ea@users.noreply.github.com>
2022-02-16 18:10:23 -0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+// import { signInWithPopup, GoogleAuthProvider, getAuth } from 'firebase/auth';
+
+const firebaseAPIKey = process.env.REACT_APP_FIRE_API_KEY
+
+const firebaseConfig = {
+  apiKey: "firebaseAPIKey",
+  authDomain: "brogey-7fb3a.firebaseapp.com",
+  projectId: "brogey-7fb3a",
+  storageBucket: "brogey-7fb3a.appspot.com",
+  messagingSenderId: "399353807646",
+  appId: "1:399353807646:web:cc92cb7bd1ed159f2ebf0f",
+  measurementId: "G-GG0ENYJVBD"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+// export const signInWithGoogle = () => {
+//   const provider = new GoogleAuthProvider();
+//   signInWithPopup(auth, provider)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     alert(err)
+//   })
+// };
+
+export { auth, db };
