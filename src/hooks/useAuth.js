b3e7da5065b5f9bd8a4744cd46a1f03625e6fef2
Eric Anderson <59844963+anderson-ea@users.noreply.github.com>
2022-02-16 18:09:07 -0700

    initial commit

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,73 @@
+import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential, signInWithPopup, signOut } from "@firebase/auth";
+import { auth } from '../firebase';
+
+const AuthContext = createContext({})
+
+const config = {
+  scope: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+};
+
+export const AuthProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loadingInitial, setLoadingInitial] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(
+    () => 
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // logged in
+          setUser(user);
+        } else {
+          // not logged in
+          setUser(null);
+        }
+      
+        setLoadingInitial(false)
+      }), 
+    []
+  );
+  const logout = () => {
+    setLoading(true);
+    signOut(auth)
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  };
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+
+    signInWithPopup(config).then(async (logInResult) => {
+      if (logInResult.type === "success") {
+        const { idToken, accessToken } = logInResult;
+        const credential = GoogleAuthProvider.credential(idToken, accessToken);
+
+        await signInWithCredential(auth, credential);
+      }
+      return Promise.reject();
+    })
+    .catch(error => setError(error))
+    .finally(() => setLoading(false));
+  };
+
+  const memoedValue = useMemo(() => ({
+    user,
+    loading,
+    error,
+    signInWithGoogle,
+    logout,
+  }), [user, loading, error])
+
+  return (
+    <AuthContext.Provider value={memoedValue}>
+      {!loadingInitial && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default function useAuth() {
+  return useContext(AuthContext);
+}
