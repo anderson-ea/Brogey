63284fd02523695e5eb64da480020c2d60d8157d
Eric Anderson <59844963+anderson-ea@users.noreply.github.com>
2022-04-06 12:19:47 -0600

    update test

--- src/components/__test__/ChatRow.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react"
-import ChatRow from "../ChatRow"
-
-test("text prompts user to send message", () => {
-  const { getByTestId } = render(<ChatRow />)
-  const messageElement = getByTestId("message")
-  // figure out how to fix errors and test
-})
