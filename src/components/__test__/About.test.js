f7a697115c999a32000c820163cee82c5f7937bb
Eric Anderson <anderson.ericandrew@gmail.com>
2022-09-21 11:44:31 -0600

    update test

--- src/components/__test__/About.test.js
+++ src/components/__test__/About.test.js
@@ -1,19 +1,42 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup, fireEvent, getByTestId } from "@testing-library/react";
 import { About } from "../About";
+import useAuth from "../useAuth";
+jest.mock("../useAuth")
 
+beforeEach(cleanup); //clean DOM
 
+jest.mock('../firebase', () => ({
+  db: {
+    firestore: jest.fn(() => ({
+      collection: jest.fn(() => ({
+        doc: jest.fn(() => ({
+          update: jest.fn()
+        })),
+      })),
+    })),
+  },
+}));
 
-test('Submit button is unavailable', () => {
+jest.mock(".../hooks", () => ({
+  useAuth: jest.fn(() => ({
+    setUser: jest.fn(),
+    user: {
+        displayName: 'Eric', 
+        uid: '1'
+    },
+  })),
+}))
 
-  const mockedUsedNavigate = jest.fn();
-  jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockedUsedNavigate,
-  }));
-
-  // working on red green testing...not working yet
+test('about test', () => {
+  const resolvedValue = {
+    user: {
+      displayName: 'Eric',
+      uid: '1'
+    }
+  }
+  useAuth.mockResolvedValueOnce(resolvedValue)
   const { getByTestId } = render(<About />)
-  const buttonElement = getByTestId("submit")
-  expect(buttonElement.toBeDisabled())
+  const buttonElement = getByTestId('submit')
+  expect(buttonElement).toBeDisabled()
 })
